11dbc7255db3755fd70d62ba64229b1bbb24b278
Lars Eschweiler <vreezy117@googlemail.com>
2023-12-27 18:30:50 +0100

    get mails

---
+++ getMails.js
@@ -0,0 +1,44 @@
+const { readFileSync } = require('fs');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+
+// first "npm install -g m365"
+// than login with "m365 login"
+// than run "node getMails.js"
+
+
+async function hhh(execString) {
+  const { stdout, stderr } = await exec(execString);
+  return stdout
+
+}
+
+async function main() {
+  // get mitglieder group
+  const x = await hhh('m365 aad group user list -i 5d19405e-7efc-4a35-9d3a-168db046766c')
+  if(x) {
+    const mitglieder = await JSON.parse(x)
+    // console.log(mitglieder)
+    const mails = []
+    for(const element of mitglieder) {
+      
+      const y = await hhh(`m365 aad user get -i ${element.id} -p "otherMails, mail"`)
+      if(y) {
+        const user = await JSON.parse(y)
+        mails.push(user.mail)
+
+        user.otherMails.forEach(mail => mails.push(mail))
+      }
+      
+    }
+
+    console.log(mails.join("; "))
+  }
+}
+
+main()
+
+
+
+
